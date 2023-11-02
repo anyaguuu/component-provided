2280023b529f5579cf361f1086657703603f5c14
anyaguuu <ag156@rice.edu>
2023-11-02 16:38:08 -0500

    checkboxes

--- src/view.ts
+++ src/view.ts
@@ -54,7 +54,49 @@ class InputForm extends HTMLElement { // SO COMPONENT = INPUTFORM!!
     }
   }
 
-  connected
+  // every time rendered
+  connectedCallback(): void {
+    const form = this.shadowRoot?.querySelector("form");
+    if (!(form instanceof HTMLFormElement)) {
+      throw new Error("form not found");
+    }
+
+    this.controller = new AbortController(); // starts this up
+    const options = { signal: this.controller.signal }; // starts this up
+
+    form.addEventListener("submit", this.handleSubmit.bind(this), options); // binds
+  }
+
+  // every time removed
+  disconnectedCallBack(): void {
+    this.controller?.abort();
+    this.controller = null;
+  }
+
+  handleSubmit(eveent: SubmitEvent) {
+    event?.preventDefault();
+    const checkboxes = this.shadowRoot?.querySelectorAll(
+      "input[type='checkbox']:checked",
+    );
+    if (checkboxes === undefined) {
+      throw new Error("checkboxes not found");
+      return;
+    }
+
+    const values: Array<string> = [];
+    for (let idx = 0; idx < checkboxes.length; idx++) {
+      const checkbox = checkboxes[idx];
+      if (checkbox instanceof HTMLInputElement) {
+        values.push(checkbox.value); // add all values of checkboxes to array
+      }
+    }
+
+    // Create and dispatch custom request activity event
+    const requestActivityEvent = new CustomEvent("requestActivityEvent", {
+      detail: {types: values},
+    });
+    document.dispatchEvent(requestActivityEvent);
+  }
 }
 
 /* A web component to display activities */
@@ -62,6 +104,34 @@ class ResultDisplay extends HTMLElement {
   /* TODO: Write the ResultDisplay component. Note that you will need a clear
    * and a displayActivity method for the View class to use.
    */
+
+  constructor() {
+    super();
+    this.attachShadow({mode: "open"});
+  }
+
+  connectedCallback(): void {
+    this.clear(); // start off blank
+  }
+
+  clear(): void {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = `
+        <style>
+          section {
+            border: 1px solid lightgray;
+            border-radius: 10px;
+            padding: 10px;
+            margin: 10px auto;
+            max-width: 40%;
+          }
+        </style>
+        <h2>Results</h2>
+      `;
+    } else {
+      throw new Error("shadowRoot does not exist");
+    }
+  }
 }
 
 customElements.define("input-form", InputForm);
